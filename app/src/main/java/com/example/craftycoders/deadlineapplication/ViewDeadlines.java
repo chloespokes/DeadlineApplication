fea9614cb0b519091f9861348744797738e751c2
chloespokes <c.spokes94@yahoo.co.uk>
2016-05-02 10:30:53 +0100

    Revert "Add Deadline"

--- app/src/main/java/com/example/craftycoders/deadlineapplication/ViewDeadlines.java
+++ app/src/main/java/com/example/craftycoders/deadlineapplication/ViewDeadlines.java
@@ -1,18 +1,13 @@
 package com.example.craftycoders.deadlineapplication;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Toast;
-
+import android.os.Bundle;
 
 public class ViewDeadlines extends AppCompatActivity {
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_deadlines);
     }
 }
