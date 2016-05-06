cfd6091b3041f628cee8fc72ab7bdd088438637b
Simon Walker <s.walker-12@student.lboro.ac.uk>
2016-05-06 11:52:34 +0100

    Content provider logic
    Added a content provider logic and a repository which holds examples of
    how to query the CP

--- app/src/main/java/com/example/craftycoders/deadlineapplication/ViewDeadlines.java
+++ app/src/main/java/com/example/craftycoders/deadlineapplication/ViewDeadlines.java
@@ -1,31 +1,35 @@
 package com.example.craftycoders.deadlineapplication;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-
-<<<<<<< HEAD
-import android.util.Log;
-=======
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
->>>>>>> master
+
 import android.widget.ListView;
 
 import com.example.craftycoders.deadlineapplication.Data.DbHelper;
+import com.example.craftycoders.deadlineapplication.Data.DeadlineProvider;
+import com.example.craftycoders.deadlineapplication.Data.DeadlinesContract;
 import com.example.craftycoders.deadlineapplication.Models.Deadline;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 
 public class ViewDeadlines extends AppCompatActivity {
 
     private DeadlineAdapter mAdapter;
     private DbHelper mDbHelper = DbHelper.getInstance(this);
+    private String TAG = "ViewDeadlinesActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,9 @@ public class ViewDeadlines extends AppCompatActivity {
         listViewDeadline.setAdapter(mAdapter);
 
         PopulateImpendingDeadlines();
+
+        //AddDeadline();
+        GetAllDeadlines();
 
     }
 
@@ -67,19 +74,94 @@ public class ViewDeadlines extends AppCompatActivity {
     }
 
     private void PopulateImpendingDeadlines() {
-
-         // TODO: 30/04/2016 call content provider to return list of impending deadlines
-        final List<Deadline> finalDeadlines;
+        final List<Deadline> finalDeadlines = GetAllDeadlines();
 
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 mAdapter.clear();
-                // TODO: 02/05/2016 uncomment when successfully retrieving data from db 
-//                for (Deadline item : finalDeadlines) {
-//                    mAdapter.add(item);
-//                }
+                for (Deadline item : finalDeadlines) {
+                    mAdapter.add(item);
+                }
             }
         });
     }
+
+    private void AddDeadline(){
+        ContentValues values = new ContentValues();
+        values.put(DeadlinesContract.KEY_ID, 10);
+        values.put(DeadlinesContract.KEY_TITLE, "MAD");
+        values.put(DeadlinesContract.KEY_NOTES, "these are notes");
+        values.put(DeadlinesContract.KEY_DUE_DATE, 3654378);
+        values.put(DeadlinesContract.KEY_LOC_LAT, 2532);
+        values.put(DeadlinesContract.KEY_LOC_LONG, 65326);
+        values.put(DeadlinesContract.KEY_HAND_IN, false);
+
+        try{
+            Uri uri = getContentResolver().insert(DeadlinesContract.CONTENT_URI, values);
+            Toast.makeText(getBaseContext(),
+                    uri.toString(), Toast.LENGTH_LONG).show();
+        }
+        catch(Exception e){
+            Log.d("ContentProvider", "AddDeadline: Failed to add ");
+        }
+    }
+
+    private List<Deadline> GetAllDeadlines(){
+        List<Deadline> allDeadlines= new ArrayList<>();
+
+
+        // A "projection" defines the columns that will be returned for each row
+        String[] mProjection =
+                {
+                        DeadlinesContract.KEY_ID,
+                        DeadlinesContract.KEY_TITLE,
+                        DeadlinesContract.KEY_NOTES,
+                        DeadlinesContract.KEY_DUE_DATE,
+                        DeadlinesContract.KEY_LOC_LAT,
+                        DeadlinesContract.KEY_LOC_LONG,
+                        DeadlinesContract.KEY_HAND_IN
+                };
+
+        // Defines a string to contain the selection clause
+        String mSelectionClause = null;
+
+        // Initializes an array to contain selection arguments
+        String[] mSelectionArgs = {""};
+
+        Cursor mCursor = getContentResolver().query(
+                DeadlinesContract.CONTENT_URI,
+                mProjection,
+                mSelectionClause,
+                mSelectionArgs,
+                null);
+
+        // Some providers return null if an error occurs, others throw an exception
+        if (null == mCursor) {
+            Log.d(TAG, "GetAllDeadlines: provider returned an error");
+        // If the Cursor is empty, the provider found no matches
+        } else if (mCursor.getCount() < 1) {
+            Log.d(TAG, "GetAllDeadlines: provider returned no results");
+        } else {
+            while (mCursor.moveToNext()) {
+
+                Deadline deadline = new Deadline();
+                deadline.setId(mCursor.getInt(mCursor.getColumnIndex(DeadlinesContract.KEY_ID)));
+                deadline.setTitle(mCursor.getString(mCursor.getColumnIndex(DeadlinesContract.KEY_TITLE)));
+                deadline.setNotes(mCursor.getString(mCursor.getColumnIndex(DeadlinesContract.KEY_NOTES)));
+                deadline.setDueDate(mCursor.getLong(mCursor.getColumnIndex(DeadlinesContract.KEY_DUE_DATE)));
+                deadline.setLocationLat(mCursor.getFloat(mCursor.getColumnIndex(DeadlinesContract.KEY_LOC_LAT)));
+                deadline.setLocationLong(mCursor.getFloat(mCursor.getColumnIndex(DeadlinesContract.KEY_LOC_LONG)));
+                deadline.setIsHandedIn(mCursor.getInt(mCursor.getColumnIndex(DeadlinesContract.KEY_HAND_IN)) > 0);
+
+                allDeadlines.add(deadline);
+            }
+        }
+
+        return allDeadlines;
+
+    }
+
+
+
 }
